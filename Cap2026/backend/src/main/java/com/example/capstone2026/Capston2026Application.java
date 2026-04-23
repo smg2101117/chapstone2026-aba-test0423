3b659862638092f222784a31c95712e457c6b10b
smg2101117 <tjalsry02@g.yju.ac.kr>
2026-04-23 16:29:37 +0900

    feat: capstone2026-aba 프로젝트 초기 설정 커밋

---
+++ Cap2026/backend/src/main/java/com/example/capstone2026/Capston2026Application.java
@@ -0,0 +1,13 @@
+package com.example.capstone2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capston2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capston2026Application.class, args);
+	}
+
+}
